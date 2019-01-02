7184ff545cf6e1d80bb3e5addfe8a2670af8bd70
roys911 <roysomak4@gmail.com>
2019-01-02 04:24:35 -0500

    Initial commit

---
+++ generate_ss_cert.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# input arg for domain name
+domain=$1
+if [ -z "$domain" ];
+    then
+        echo "Please provide a domain name. It is a required input."
+        exit
+fi
+
+# create output directory
+if [ ! -d "$domain" ];
+    then
+        echo "Creating domain directory for $domain..."
+        mkdir -p $domain
+    else
+        echo "Domain directory found."
+fi
+
+# create crt and key files
+echo "Generating self-signed certificates for $domain..."
+openssl req -newkey rsa:4096 \
+            -nodes \
+            -sha256 \
+            -keyout $domain/server.key \
+            -x509 \
+            -days 3650 \
+            -out $domain/server.crt \
+            -subj "/C=US/ST=PA/L=PA/O=IT/CN=$domain"
+
+# create dhparam file
+openssl dhparam -out $domain/dhparam.pem 4096
